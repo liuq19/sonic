a5b891e179e3b1c9462c96c829c9415529bc5b8e
liuqiang <liuqiang.06@bytedance.com>
2026-06-01 18:33:10 +0800

    fix: align map runtime tags with Go 1.24

--- internal/rt/map_go126.go
+++ internal/rt/map_go126.go
@@ -1,5 +1,5 @@
-//go:build go1.26 || goexperiment.swissmap
-// +build go1.26 goexperiment.swissmap
+//go:build go1.24
+// +build go1.24
 
 package rt
 
@@ -7,6 +7,7 @@ import (
 	"unsafe"
 )
 
+// Go 1.24 and later use the SwissMap iterator layout by default.
 type GoMapIterator struct {
 	K  unsafe.Pointer
 	V  unsafe.Pointer
